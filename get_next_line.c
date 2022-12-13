b7c0825bdede572aa2250f3919c8797b3bb71999
Clement LACAILLE <clacaill@e3r9p4.clusters.42paris.fr>
2022-12-13 14:50:21 +0100

    marche avec

--- get_next_line.c
+++ get_next_line.c
@@ -2,47 +2,42 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void	read_and_print_100(int fd)
-{
-	char	buf[100];	// stocke les caractères lus par read
-	int	nb_read;	// stocke le retour de read
-
-
-//	Lit 100 caractères du fichier
-	nb_read = read(fd, buf, 100);
-//	En cas d'erreur, on arrête tout
-	if (nb_read == -1)
-	{
-		printf("Erreur de lecture !\n");
-		return ;
-	}
-//	Ajoute le \0 à la fin de la chaîne
-	buf[nb_read] = '\0';
-//	Imprime ce que contient le buffer après la lecture, précédé
-//	du déscripteur de fichier d'où provient le contenu
-	printf("\e[36mfd %d : [\e[0m%s\e[36m]\e[0m\n", fd, buf);
-	return ;
-}
-
 int	main(void)
 {
-	int fd1;	// descripteur de fichier à lire
-	int fd2;	// second descripteur de fichier à lire
+	char	buf[100];	// stocke les caractères lus par read
+	int	fd;		// descripteur de fichier à lire
+	int	nb_read;	// stocke le retour de read
+	int	count;		// compte du nombre de lectures avec read
 
 //	Ouvre le fichier cat.txt en mode lecture seule
-	fd1 = open("cat.txt", O_RDONLY);
-//	Ouvre à nouveau le fichier cat.txt en mode lecture seule
-	fd2 = open("cat.txt", O_RDONLY);
-	if (fd1 == -1 || fd2 == -1)
+	fd = open("cat.txt", O_RDONLY);
+	if (fd == -1)
 		return (1);
-//	Imprime les 100 premiers caractères du descripteur 1
-	read_and_print_100(fd1);
-//	Imprime les 100 prochains caractères du descripteur 1
-	read_and_print_100(fd1);
-//	Imprime les 100 premiers caractères du descripteur 2
-	read_and_print_100(fd2);
-//	Ferme les descripteurs de fichier ouverts plut tôt
-	close(fd1);
-	close(fd2);
+//	Initialise les variables de compte
+	nb_read = -1;
+	count = 0;
+//	Boucle tant que read ne retourne pas 0 (ce qui veut dire
+//	qu'il n'y a plus rien à lire dans le fichier)
+	while (nb_read != 0)
+	{
+		// Lecture de 100 caractères avec read depuis le
+		// descripteur de fichier ouvert
+		nb_read = read(fd, buf, 100);
+		// En cas d'erreur, read renvoie -1, on arrête tout
+		if (nb_read == -1)
+		{
+			printf("Erreur de lecture !\n");
+			return (1);
+		}
+		// Read n'ajoute pas le \0 à la fin de la chaîne
+		// de caractères lus. On peut se servir du nombre
+		// de caractères lus comme index du dernier caractère
+		buf[nb_read] = '\0';
+		// Imprime ce que contient le buffer après la lecture
+		printf("\e[36m%d : [\e[0m%s\e[36m]\e[0m\n", count, buf);
+		count++;
+	}
+//	Ferme le descripteur de fichier ouvert plus tôt
+	close(fd);
 	return (0);
 }
