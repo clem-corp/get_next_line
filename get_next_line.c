1e17de1f1cde2df7522ba563b448037f3eb16465
clem-corp <clement2.lacaille@gmail.com>
2022-12-16 20:28:23 +0100

    ammmnnn

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: clacaill <clacaill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 16:15:30 by clacaill          #+#    #+#             */
-/*   Updated: 2022/12/15 19:17:34 by clacaill         ###   ########.fr       */
+/*   Updated: 2022/12/16 20:27:05 by clacaill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,24 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, buff, 0) == -1)
 		return (NULL);
-	
 	nb_read = -1;
 	line = "";
-	line = ft_strjoin(line , buff);
+	line = ft_strjoin(line, buff);
 	while (!ft_strchr(line, '\n') && nb_read != 0)
 	{
 		nb_read = read(fd, buff, BUFFER_SIZE);
-		if(nb_read == -1)
+		if (nb_read == -1)
 			return (NULL);
 		buff[nb_read] = '\0';
 		tmp = line;
-		line = ft_strjoin(tmp , buff);
+		line = ft_strjoin(tmp, buff);
 		free(tmp);
 	}
-	if(ft_strchr(line, '\n'))
+	if (ft_strchr(line, '\n'))
 	{
 		int i = 0;
 		int j = 0;
-		while(line[i] && line[i] != '\n')
+		while (line[i] && line[i] != '\n')
 			i++;
 		while (line[j + i] && line[j + i + 1])
 		{
@@ -47,10 +46,10 @@ char	*get_next_line(int fd)
 			j++;
 		}
 		buff[j] = '\0';
-		if(line[i])
+		if (line[i])
 			line[i + 1] = '\0';
 	}
-	if(line[0] == '\0')
+	if (line[0] == '\0')
 	{
 		free(line);
 		return (NULL);
